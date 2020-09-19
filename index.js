613bc7fbff04a7f2d214ae6fb43c421da4b3102e
Pil <pilato.arnaud@gmail.com>
2020-09-19 22:10:33 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+
+  /*
+
+    🏁 Please take this quick challenge and rename carefully all the variable, and fix this broken code by assigning the correct 
+    variable keyword you have learned in the quest.
+    ex: const name = "David";
+
+  */
+
+  let firstName = "David";
+  let lastName = "Bruno";
+  let profile = "Hi, I'm David Bruno from SF, I like to cook and meet new people.";
+
+  let imgProfile = "http://www.go.com/david.png";
+
+  let city = "San Francisco";
+  let abstract = "CA";
+
+  let people_follower = 109;
+  let people_following = 1500 + 1;
+
+
+  const whatTimeIsIt = new Date();
+
+  /*
+
+    🏁 David is following one more account, increment the total of following account
+
+  */
+
